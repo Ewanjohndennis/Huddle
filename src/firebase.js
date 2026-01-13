ea4e54aa045ed80d36a0cb85f08101f7bc710070
Ewanjohndennis <ewandennis2006@gmail.com>
2026-01-13 19:46:48 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBAz32A7HTq4lhiaKTZFtQMJB5NgNriEh4",
+ apiKey: "AIzaSyBAz32A7HTq4lhiaKTZFtQMJB5NgNriEh4",
   authDomain: "huddle-chat-6dfb7.firebaseapp.com",
   projectId: "huddle-chat-6dfb7",
+  storageBucket: "huddle-chat-6dfb7.firebasestorage.app",
+  messagingSenderId: "450832618076",
+  appId: "1:450832618076:web:87c4ef1af5497c5bd08c99"
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
